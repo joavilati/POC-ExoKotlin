06a9b3d9ebb458f8fcb3a7b63e070f9d3271be2a
Jhonata <jhonata.avila@progit.com.br>
2020-11-18 15:43:00 -0300

    feat(All): aqui ja foi

--- app/src/main/java/com/example/poc_exokotlin/AudioPlayerService.kt
+++ app/src/main/java/com/example/poc_exokotlin/AudioPlayerService.kt
@@ -1,5 +1,6 @@
 package com.example.poc_exokotlin
 
+import android.app.Notification
 import android.app.PendingIntent
 import android.app.Service
 import android.content.Context
@@ -10,7 +11,11 @@ import android.os.Binder
 import android.os.IBinder
 import android.support.v4.media.session.MediaSessionCompat
 import android.util.Log
+import androidx.annotation.MainThread
 import androidx.core.graphics.drawable.toBitmap
+import androidx.lifecycle.LifecycleService
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.google.android.exoplayer2.*
 import com.google.android.exoplayer2.analytics.AnalyticsListener
 import com.google.android.exoplayer2.audio.AudioAttributes
@@ -24,152 +29,278 @@ import com.google.android.exoplayer2.util.Util
 
 private const val TAG = "AudioPlayerService"
 
+private const val ARG_URI = "uri_string"
+private const val ARG_TITLE = "title"
+private const val PLAYBACK_CHANNEL_ID = "playback_channel"
+private const val PLAYBACK_NOTIFICATION_ID = 1
+
 class AudioPlayerService : Service() {
 
+    private lateinit var player: SimpleExoPlayer
+    private lateinit var playerNotificationManager: PlayerNotificationManager
+    private val mBinder: IBinder = AudioServiceBinder()
+
+    inner class AudioServiceBinder : Binder() {
+        val service
+            get() = this@AudioPlayerService
+
+        val player
+            get() = this@AudioPlayerService.player
+    }
+
     companion object {
-        const val PLAYBACK_CHANNEL_ID = "playback_channel"
-        const val PLAYBACK_NOTIFICATION_ID = 1
-    }
-
-    private var player: SimpleExoPlayer? = null
-    private var playerNotificationManager: PlayerNotificationManager? = null
-    private var mediaSession: MediaSessionCompat? = null
-    private var mediaSessionConnector: MediaSessionConnector? = null
-    val context: Context = this
-    private val mBinder: IBinder = LocalBinder()
-
-    inner class LocalBinder : Binder() {
-        //Classe utilizada no bind do player do serviço com o player da view
-        fun getBinder(): AudioPlayerService {
-            return this@AudioPlayerService
+        @MainThread
+        fun newIntent(
+            context: Context,
+            title: String,
+            uriString: String
+        ): Intent {
+            return Intent(context, AudioPlayerService::class.java).apply {
+                putExtra(ARG_TITLE, title)
+                putExtra(ARG_URI, uriString)
+            }
         }
     }
 
-    private fun startPlayer() {
-        // Inicialização do player e dos demais metodos
-        Log.e("Ninja", "Teste logs")
-        val dataSourceFactory = DefaultHttpDataSourceFactory( Util.getUserAgent(context, getString(R.string.app_name)))
+    override fun onCreate() {
+        super.onCreate()
 
-        player = SimpleExoPlayer.Builder(context).setTrackSelector(DefaultTrackSelector(context))
-            .build() //Constroi um player
-        //Daqui pra baixo configura o gerenciador do player em notificação
-        buildSingleMediaSource(dataSourceFactory) //se esse estiver sendo chamado comentar o buildConcatMediaSources
-        //  buildConcatMediaSources(dataSourceFactory) //se esse estiver sendo chamado comentar o buildSingleMediaSource
-        configNotificationManager()
-        configAudioFocus()
-        addListeners()
-        playerNotificationManager?.setPlayer(player)
-        player?.playWhenReady = true
-    }
+        player = SimpleExoPlayer.Builder(this).setTrackSelector(DefaultTrackSelector(this)).build()
 
-    private fun addListeners() {
+        val audioAtribute = AudioAttributes.Builder()
+            .setUsage(C.USAGE_MEDIA)
+            .setContentType(C.CONTENT_TYPE_SPEECH)
+            .build()
+        player.audioAttributes = audioAtribute
 
-        player?.apply {
-            addListener(listeners)
-        }
-    }
-
-    private fun buildSingleMediaSource(dataSourceFactory: DefaultHttpDataSourceFactory) {
-        // LINK DA RADIO
-        val uri =
-            Uri.parse("https://s2.radio.co/sdf9aeb4e9/listen")
-        val mediaSource = ProgressiveMediaSource.Factory(dataSourceFactory).createMediaSource(uri)
-        player?.prepare(mediaSource) //Alimenta o player com a playlist
-    }
-
-
-    private fun configNotificationManager() {
         playerNotificationManager = PlayerNotificationManager.createWithNotificationChannel(
-            context,
+            applicationContext,
             PLAYBACK_CHANNEL_ID,
             R.string.playback_channel_name,
-            R.string.playback_channel_description,
             PLAYBACK_NOTIFICATION_ID,
             object : PlayerNotificationManager.MediaDescriptionAdapter {
+                override fun getCurrentContentTitle(player: Player): String = "Titulo aqui"
+
+
                 override fun createCurrentContentIntent(player: Player): PendingIntent? {
-                    return null
+                    return PendingIntent.getActivity(
+                        applicationContext,
+                        0,
+                        Intent(applicationContext, MainActivity::class.java),
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                    )
                 }
 
-                override fun getCurrentContentText(player: Player): String? {
-                    return "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
-                }
-
-                override fun getCurrentContentTitle(player: Player): String {
-                    return "Teste Titulo"
-                }
+                override fun getCurrentContentText(player: Player): String? = "Conteudo aqui!!!!!"
 
                 override fun getCurrentLargeIcon(
                     player: Player,
                     callback: PlayerNotificationManager.BitmapCallback
                 ): Bitmap? {
-                    return context.resources.getDrawable(R.drawable.penguim).toBitmap()
+                    return applicationContext.resources.getDrawable(R.drawable.penguim).toBitmap()
                 }
+
             },
             object : PlayerNotificationManager.NotificationListener {
-                override fun onNotificationCancelled(
+                override fun onNotificationStarted(
                     notificationId: Int,
-                    dismissedByUser: Boolean
+                    notification: Notification
                 ) {
-                    super.onNotificationCancelled(notificationId, dismissedByUser)
-                    stopSelf()
+                    startForeground(notificationId, notification)
+                }
 
+                override fun onNotificationCancelled(notificationId: Int) {
+                    stopSelf()
+                    stopForeground(true)
+                }
+
+                override fun onNotificationPosted(
+                    notificationId: Int,
+                    notification: Notification,
+                    ongoing: Boolean
+                ) {
+                    if (ongoing) {
+                        // Make sure the service will not get destroyed while playing media.
+                        startForeground(notificationId, notification)
+                    } else {
+                        // Make notification cancellable.
+                        stopForeground(false)
+                    }
                 }
             }
-        )
-    }
-
-    private fun configAudioFocus() { //Configura o Foco do audio para modo de discurso pausando e resumindo automaticamente a reprodução quando recebida uma notificação
-        val audioAtribute = AudioAttributes.Builder()
-            .setUsage(C.USAGE_MEDIA)
-            .setContentType(C.CONTENT_TYPE_MUSIC)
-            .build()
-        player?.audioAttributes = audioAtribute
-    }
-
-    fun getPlayerInstance(): SimpleExoPlayer? {
-        if (player == null) {
-            startPlayer()
+        ).apply {
+            // Omit skip previous and next actions.
+            setUseNavigationActions(false)
+            // Add stop action.
+            setUseStopAction(true)
+            setPlayer(player)
         }
-        return player
     }
 
-    override fun onBind(intent: Intent?): IBinder? =  mBinder
+    @MainThread
+    fun play(uri: Uri) {
+        val userAgent = Util.getUserAgent(applicationContext, BuildConfig.APPLICATION_ID)
+        val dataSourceFactory = DefaultHttpDataSourceFactory(userAgent)
+        val mediaSource = ProgressiveMediaSource.Factory(dataSourceFactory).createMediaSource(uri)
 
-
-    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int =  START_STICKY
+        player.prepare(mediaSource)
+        player.playWhenReady = true
+    }
 
     override fun onDestroy() {
-        mediaSession?.release()
-        mediaSessionConnector?.setPlayer(null, null)
-        playerNotificationManager?.setPlayer(null)
-        player?.release()
-        player = null
         super.onDestroy()
+        playerNotificationManager.setPlayer(null)
     }
 
-    val listeners = object : Player.EventListener {
-        override fun onPlayerError(error: ExoPlaybackException) {
-            super.onPlayerError(error)
-            Log.e(TAG, "$error")
-
-            when (error.type) {
-                ExoPlaybackException.TYPE_OUT_OF_MEMORY -> {
-                    Log.e(TAG, "TYPE__OUT_OF_MEMORY: ${error.outOfMemoryError.message}")
-                }
-                ExoPlaybackException.TYPE_REMOTE -> {
-                    Log.e(TAG, "TYPE_REMOTE: ${error.message}")
-                }
-                ExoPlaybackException.TYPE_RENDERER -> {
-                    Log.e(TAG, "TYPE_RENDERER: ${error.rendererException.message}")
-                }
-                ExoPlaybackException.TYPE_SOURCE -> {
-                    Log.e(TAG, "TYPE_SOURCE: ${error.sourceException.message}")
-                }
-                ExoPlaybackException.TYPE_UNEXPECTED -> {
-                    Log.e(TAG, "TYPE_UNEXPECTED${error.unexpectedException}")
-                }
-            }
-        }
-
-    }
+    override fun onBind(intent: Intent?): IBinder? = mBinder
 }
+
+//class AudioPlayerService : Service() {
+//
+//    companion object {
+//        const val PLAYBACK_CHANNEL_ID = "playback_channel"
+//        const val PLAYBACK_NOTIFICATION_ID = 1
+//    }
+//
+//    private var player: SimpleExoPlayer? = null
+//    private var playerNotificationManager: PlayerNotificationManager? = null
+//    private var mediaSession: MediaSessionCompat? = null
+//    private var mediaSessionConnector: MediaSessionConnector? = null
+//    val context: Context = this
+//    private val mBinder: IBinder = LocalBinder()
+//
+//    inner class LocalBinder : Binder() {
+//        //Classe utilizada no bind do player do serviço com o player da view
+//        fun getBinder(): AudioPlayerService {
+//            return this@AudioPlayerService
+//        }
+//    }
+//
+//    private fun startPlayer() {
+//        // Inicialização do player e dos demais metodos
+//        Log.e("Ninja", "Teste logs")
+//        val dataSourceFactory = DefaultHttpDataSourceFactory( Util.getUserAgent(context, getString(R.string.app_name)))
+//
+//        player = SimpleExoPlayer.Builder(context).setTrackSelector(DefaultTrackSelector(context))
+//            .build() //Constroi um player
+//        //Daqui pra baixo configura o gerenciador do player em notificação
+//        buildSingleMediaSource(dataSourceFactory) //se esse estiver sendo chamado comentar o buildConcatMediaSources
+//        //  buildConcatMediaSources(dataSourceFactory) //se esse estiver sendo chamado comentar o buildSingleMediaSource
+//        configNotificationManager()
+//        configAudioFocus()
+//        addListeners()
+//        playerNotificationManager?.setPlayer(player)
+//        player?.playWhenReady = true
+//    }
+//
+//    private fun addListeners() {
+//
+//        player?.apply {
+//            addListener(listeners)
+//        }
+//    }
+//
+//    private fun buildSingleMediaSource(dataSourceFactory: DefaultHttpDataSourceFactory) {
+//        // LINK DA RADIO
+//        val uri =
+//            Uri.parse("https://s2.radio.co/sdf9aeb4e9/listen")
+//        val mediaSource = ProgressiveMediaSource.Factory(dataSourceFactory).createMediaSource(uri)
+//        player?.prepare(mediaSource) //Alimenta o player com a playlist
+//    }
+//
+//
+//    private fun configNotificationManager() {
+//        playerNotificationManager = PlayerNotificationManager.createWithNotificationChannel(
+//            context,
+//            PLAYBACK_CHANNEL_ID,
+//            R.string.playback_channel_name,
+//            R.string.playback_channel_description,
+//            PLAYBACK_NOTIFICATION_ID,
+//            object : PlayerNotificationManager.MediaDescriptionAdapter {
+//                override fun createCurrentContentIntent(player: Player): PendingIntent? {
+//                    return null
+//                }
+//
+//                override fun getCurrentContentText(player: Player): String? {
+//                    return "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+//                }
+//
+//                override fun getCurrentContentTitle(player: Player): String {
+//                    return "Teste Titulo"
+//                }
+//
+//                override fun getCurrentLargeIcon(
+//                    player: Player,
+//                    callback: PlayerNotificationManager.BitmapCallback
+//                ): Bitmap? {
+//                    return context.resources.getDrawable(R.drawable.penguim).toBitmap()
+//                }
+//            },
+//            object : PlayerNotificationManager.NotificationListener {
+//                override fun onNotificationCancelled(
+//                    notificationId: Int,
+//                    dismissedByUser: Boolean
+//                ) {
+//                    super.onNotificationCancelled(notificationId, dismissedByUser)
+//                    stopSelf()
+//
+//                }
+//            }
+//        )
+//    }
+//
+//    private fun configAudioFocus() { //Configura o Foco do audio para modo de discurso pausando e resumindo automaticamente a reprodução quando recebida uma notificação
+//        val audioAtribute = AudioAttributes.Builder()
+//            .setUsage(C.USAGE_MEDIA)
+//            .setContentType(C.CONTENT_TYPE_MUSIC)
+//            .build()
+//        player?.audioAttributes = audioAtribute
+//    }
+//
+//    fun getPlayerInstance(): SimpleExoPlayer? {
+//        if (player == null) {
+//            startPlayer()
+//        }
+//        return player
+//    }
+//
+//    override fun onBind(intent: Intent?): IBinder? =  mBinder
+//
+//
+//    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int =  START_STICKY
+//
+//    override fun onDestroy() {
+//        mediaSession?.release()
+//        mediaSessionConnector?.setPlayer(null, null)
+//        playerNotificationManager?.setPlayer(null)
+//        player?.release()
+//        player = null
+//        super.onDestroy()
+//    }
+//
+//    val listeners = object : Player.EventListener {
+//        override fun onPlayerError(error: ExoPlaybackException) {
+//            super.onPlayerError(error)
+//            Log.e(TAG, "$error")
+//
+//            when (error.type) {
+//                ExoPlaybackException.TYPE_OUT_OF_MEMORY -> {
+//                    Log.e(TAG, "TYPE__OUT_OF_MEMORY: ${error.outOfMemoryError.message}")
+//                }
+//                ExoPlaybackException.TYPE_REMOTE -> {
+//                    Log.e(TAG, "TYPE_REMOTE: ${error.message}")
+//                }
+//                ExoPlaybackException.TYPE_RENDERER -> {
+//                    Log.e(TAG, "TYPE_RENDERER: ${error.rendererException.message}")
+//                }
+//                ExoPlaybackException.TYPE_SOURCE -> {
+//                    Log.e(TAG, "TYPE_SOURCE: ${error.sourceException.message}")
+//                }
+//                ExoPlaybackException.TYPE_UNEXPECTED -> {
+//                    Log.e(TAG, "TYPE_UNEXPECTED${error.unexpectedException}")
+//                }
+//            }
+//        }
+//
+//    }
+//}
--- app/src/main/java/com/example/poc_exokotlin/MainActivity.kt
+++ app/src/main/java/com/example/poc_exokotlin/MainActivity.kt
@@ -4,19 +4,23 @@ import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.content.ServiceConnection
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
 import android.os.IBinder
-import com.example.poc_exokotlin.AudioPlayerService.LocalBinder
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.poc_exokotlin.AudioPlayerService.AudioServiceBinder
 import com.example.poc_exokotlin.databinding.ActivityMainBinding
+import com.google.android.exoplayer2.SimpleExoPlayer
 import com.google.android.exoplayer2.util.Util
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private lateinit var mIntent: Intent
+
     private var bound = false
-    private lateinit var mService: AudioPlayerService
+    private var mService: AudioPlayerService? = null
+    private lateinit var player: SimpleExoPlayer
 
     private val mConnection = object : ServiceConnection {
         override fun onServiceDisconnected(name: ComponentName?) {
@@ -24,8 +28,9 @@ class MainActivity : AppCompatActivity() {
         }
 
         override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
-            val binder = service as LocalBinder
-            mService = binder.getBinder()
+            val binder = service as AudioServiceBinder
+            mService = binder.service
+            player = binder.player
             bound = true
             initializePlayer()
         }
@@ -36,25 +41,28 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.playerView.useController = true
-        binding.playerView.showController()
-        binding.playerView.controllerAutoShow = true
-        binding.playerView.controllerHideOnTouch = false
-       //Inicializa o Serviço em Foreground que permite que o app execute o Serviço na notificação de controle
+        registerClickEvents()
 
-        mIntent = Intent(this, AudioPlayerService::class.java) // Prepara um intent para inicializar um Serviço Foreground/Background
-        bindService(mIntent, mConnection, Context.BIND_AUTO_CREATE) //Conecta de fato a Activity com o Serviço do player
-        initializePlayer()
+        AudioPlayerService.newIntent(this, "Teste", "https://fps5.listen2myradio.com:2199/listen.php?ip=78.129.139.48&port=9360&type=s1").also {intent ->
+            Util.startForegroundService(this, intent)
+            bindService(intent, mConnection, Context.BIND_AUTO_CREATE)
+        }
     }
 
     private fun initializePlayer() { // Inicializa um player para conectar com a view
         if (bound) {
-            binding.playerView.player = mService.getPlayerInstance()
+            mService?.play(Uri.parse("https://fps5.listen2myradio.com:2199/listen.php?ip=78.129.139.48&port=9360&type=s1"))
         }
     }
 
-    override fun onStart() {
-        super.onStart()
-        Util.startForegroundService(this, mIntent)
+    private fun registerClickEvents() {
+        binding.btnPlay.setOnClickListener {
+            mService?.let {
+                mService?.play(Uri.parse("https://fps5.listen2myradio.com:2199/listen.php?ip=78.129.139.48&port=9360&type=s1"))
+            } ?: run{
+                Toast.makeText(this, "ESPERA Aê!!", Toast.LENGTH_LONG).show()
+            }
+        }
     }
+
 }
